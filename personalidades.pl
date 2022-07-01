d4b575babc20c6f816470ebb477a9d3033251886
lspigariol <lspigariol@gmail.com>
2022-07-01 10:18:32 -0300

    confechas

--- personalidades.pl
+++ personalidades.pl
@@ -1,7 +1,7 @@
 murio(juanDomingo, fecha(1, 7, 1974)).
 murio(jesusDeNazareth, fecha(3, 3, 33)).
 murio(diego,fecha(25,11,2020)).
-murio(donJulio,fecha(3,12,2020)).
+murio(donJulio,fecha(3,8,2020)).
 %murio(yabran,??).
 %
 fechaActual(fecha(1,7,2022)).
@@ -13,6 +13,9 @@ tiempoDesdeSuMuerte(Persona,Tiempo):-
     anio(FechaActual,AnioActual),
     Tiempo is AnioActual - AnioDefuncion.
 
+
+anio(fecha(_,_,A),A ).
+
 aniversariosEnAgosto(Alguien):-
     murio(Alguien,fecha(_,8,_)).
 
@@ -20,6 +23,11 @@ murioAntes(Fulano,Mengano):-
     murio(Fulano,F1),
     murio(Mengano,F2),
     esAntes(F1,F2).
+
+esAntes(F1,F2):-
+    anio(F1,A1),
+    anio(F2,A2),
+    A1 < A2.
 
 
 todosLosPrimerosDeMesHayAniversarios...
